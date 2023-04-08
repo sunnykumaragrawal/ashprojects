c2750e83c39864d9b8ef5b2722d80cb0420847ff
sunnykumaragrawal <sunny.agarwal865@gmail.com>
2023-04-08 14:19:55 +0530

    pm landing

--- resources/views/packers/demos.blade.php
+++ resources/views/packers/demos.blade.php
@@ -52,6 +52,40 @@
     </div>
   </header><!-- End #header -->
 
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6 order-lg-1 order-2">
+          <div class="owl-carousel hero-carousel">
+            <div class="hero-content">
+              <p>AGRAWAL SOFTWARE HOUSE</p>
+              <h1>We provide website for your Business starts at 129rs/mo</h1>
+              <a href="tel:917770951212" class="template-btn1 hero-btn1">Call now</a>
+              <a href="https://api.whatsapp.com/send?phone=917770951212" class="template-btn2 hero-btn2">Chat with me</a>
+            </div>
+            <div class="hero-content">
+              <p>AGRAWAL SOFTWARE HOUSE</p>
+              <h1>We create website to grow your business</h1>
+              <a href="https://agrawalsoftwarehouse.com/#services" class="template-btn1 hero-btn1">View Services</a>
+              <a href="https://agrawalsoftwarehouse.com/#pricing" class="template-btn2 hero-btn2">Check Pricing</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 order-lg-2 order-1">
+          <div class="owl-carousel heroimg-carousel">
+            <div class="hero-img">
+              <img src="{{asset('assets/img/hero/hero1.webp')}}" class="img-fluid">
+            </div>
+            <div class="hero-img">
+              <img src="{{asset('assets/img/hero/hero3.webp')}}" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- #hero -->
+
   <main class="section-bg">
     <section>
       <div class="container">
@@ -106,12 +140,92 @@
   </main>
 
   <!-- ======= Footer ======= -->
+  <!-- ======= Footer ======= -->
   <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6">
+            <div class="d-flex">
+              <h5>Top Services</h5>
+            </div>
+            <div class="d-flex ">
+              <ul>
+                <li><a href="">Web Design</a></li>
+                <li><a href="">Web Development</a></li>
+                <li><a href="">Web Support</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-6">
+            <div class="d-flex">
+              <h5>Quick Links</h5>
+            </div>
+            <div class="d-flex ">
+              <ul>
+                <li><a href="https://agrawalsoftwarehouse.com/#hero">Home</a></li>
+                <li><a href="https://agrawalsoftwarehouse.com/#services">Services</a></li>
+                <li><a href="https://agrawalsoftwarehouse.com/#pricing">Pricing</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="d-flex">
+              <h5>Templates</h5>
+            </div>
+            <div class="d-flex ">
+              <ul>
+                <li><a href="https://templates.agrawalsoftwarehouse.com/packers-movers">Packers & Movers</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6">
+            <div class="d-flex">
+              <h5>Working Hours</h5>
+            </div>
+            <div class="d-flex ">
+              <ul>
+                <li><a href="">Mon - Fri</a></li>
+                <li><a href="">Sat</a></li>
+                <li><a href="">Sun</a></li>
+              </ul>
+              <ul class="ml-3 ml-md-5">
+                <li><a href="">09:00 AM - 05:30 PM</a></li>
+                <li><a href="">09:00 AM - 01:30 PM</a></li>
+                <li><a href="">Closed</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center align-items-lg-baseline">
+          <div class="social-box">
+            <a href="https://www.facebook.com/agrawalsoftwarehouse/" target="_blank" class="facebook-bg"><i class="icofont-facebook"></i></a>
+            <a href="https://www.linkedin.com/company/agrawal-software-house/" target="_blank" class="twitter-bg"><i class="icofont-linkedin"></i></a>
+            <a href="https://www.Instagram.com/agrawalsoftware" target="_blank" class="instagram-bg"><i class="icofont-instagram"></i></a>
+          </div>
+          <div class="form_container mt-5">
+            <form action="">
+              <label for="subscribeMail">
+                Newsletter
+              </label>
+              <input
+                type="email"
+                placeholder="Enter Your email"
+                id="subscribeMail"
+              />
+              <button type="submit">
+                Subscribe
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="container-fluid copyright">
       <p>Designed By: <a href="https://agrawalsoftwarehouse.com/" target="_blank">Agrawal Software House</a></p>
     </div>
   </footer>
-
+  
   <!-- Vendor JS Files -->
   <script src="{{asset('assets/vendor/jquery/jquery.min.js')}}"></script>
   <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
